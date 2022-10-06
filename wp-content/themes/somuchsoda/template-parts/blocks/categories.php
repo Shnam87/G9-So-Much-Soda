3abb1109828fdd1adb91298801db1939ff6ece49
Mimmi <mimmizlnilsson@gmail.com>
2022-10-06 15:32:28 +0200

    categories

--- wp-content/themes/somuchsoda/template-parts/blocks/categories.php
+++ wp-content/themes/somuchsoda/template-parts/blocks/categories.php
@@ -7,28 +7,6 @@
 
 
 
-<?php
-    // verify that this is a product category page
-if ( is_product_category() ){
-    global $wp_query;
-
-    // get the query object
-    $cat = $wp_query->get_queried_object();
-
-    // get the thumbnail id using the queried category term_id
-    $thumbnail_id = get_term_meta($value->term_id, 'thumbnail_id', true); 
-
-    // get the image URL
-    $image = wp_get_attachment_url( $thumbnail_id ); 
-
-    
-
-    // print the IMG HTML
-    echo "<img src='{$image}' alt='' width='762' height='365' />";
-}
-
-?>
-
 
         <?php 
         $categories = get_field('categories');
@@ -38,10 +16,22 @@ if ( is_product_category() ){
         <?php if( $categories ): ?>
             <?php foreach( $categories as $category ): ?>
                 <div class="prod-cat-list-item">
-                    <img class="prod-cat-item-img" src="<?php echo esc_html( $category->thumbnail); ?>" alt="">
-                    <h1 class="prod-cat-item-h1"><?php echo esc_html( $category->name ); ?></h1>
-                    <p class="prod-cat-item-p"><?php echo esc_html( $category->description ); ?></p>
-                    <a class="prod-cat-item-link" href="<?php echo esc_url( get_term_link( $category ) ); ?>">View '<?php echo esc_html( $category->name ); ?>'</a>
+                    <?php 
+                        // get the thumbnail id using the queried category term_id
+                        $thumbnail_id = get_term_meta($category->term_id, 'thumbnail_id', true); 
+
+                        // get the image URL
+                        $image = wp_get_attachment_url( $thumbnail_id ); 
+
+                        // print the IMG HTML
+                        echo "<img class='prod-cat-item-img' src='{$image}' alt=''/>";
+                    ?>
+
+                    <div class="prod-cat-content-container">
+                        <h1 class="prod-cat-item-h1"><?php echo esc_html( $category->name ); ?></h1>
+                        <p class="prod-cat-item-p"><?php echo esc_html( $category->description ); ?></p>
+                        <a class="prod-cat-item-link" href="<?php echo esc_url( get_term_link( $category ) ); ?>">View '<?php echo esc_html( $category->name ); ?>'</a>
+                    </div>
                 </div>
             <?php endforeach; ?>
         <?php endif; ?>
